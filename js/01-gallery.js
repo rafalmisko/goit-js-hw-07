0e3c6432eee54dda98a24376cba2a19a94b14df8
Rafał Miśkowiec <sbgrafo@gmail.com>
2023-05-01 10:09:40 +0200

    updated files

---
+++ js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+//console.log(galleryItems);
+
+
+//1. Tworzenie i renderowanie znacznika zgodnie z tablicą danych galleryItems i dostarczonym szablonem elementu galerii.
+// tablica.map((element, index, array) => {
+//   // Ciało funkcji zwrotnej
+// });
+const galleryPics = document.querySelector('.gallery');  // if querySelector peError: galleryPics.insertAdjacentHTML is not a function 
+const mainGallery = galleryItems
+      .map (({preview, original, description}) => 
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                />
+            </a>
+        </div>`
+
+      )
+      .join(''); 
+
+galleryPics.insertAdjacentHTML('afterbegin', mainGallery); 
+
+
+    const showModalWindow = (event) => {
+      event.preventDefault();
+      if (event.target !== 'IMG') {
+        return;
+      }
+      const instance = basicLightbox.create(`<img src="{event.target.dataset.source}" 
+      width="800" height="600">`);
+      instance.show();
+
+      document.addEventListener('keyup', event => {
+        if (event.code === 'Escape') {
+          instance.close(); 
+        }
+      
+      });
+
+    }
+
+     galleryPic.addEventListener("click", showModalWindow);
---
+++ js/02-lightbox.js
@@ -0,0 +1,34 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryPics = document.querySelector('.gallery');
+const mainGallery = galleryItems
+    .map (({preview, original, description}) => 
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                />
+            </a>
+        </div>`
+
+      )
+      .join(''); 
+
+galleryPics.insertAdjacentHTML('afterbegin', mainGallery);
+
+ 
+
+const newLightboxGallery = new SimpleLightbox('.gallery a', {
+    captionData: 'alt',
+    canimationSpeed: 250,
+    captionPosition: 'bottom',
+});
+galleryPics.addEventListener("click",lightbox); 
+	 
+ 
